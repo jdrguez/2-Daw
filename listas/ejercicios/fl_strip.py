fff109cc2ada01f6982e3e107fb8c9b6acab4a4c
Jose Domingo Rodriguez <josedomingo.rguez.rguez@gmail.com>
2023-12-07 23:09:29 +0000

    Se añade fl_strip y se ha hecho

---
+++ listas/ejercicios/fl_strip.py
@@ -0,0 +1,14 @@
+# ****************
+# TROCEADO EXTREMO
+# ****************
+
+
+def run(numbers: str) -> str:
+    # TU CÓDIGO AQUÍ
+    strip_numbers = 'output'
+
+    return strip_numbers
+
+
+if __name__ == '__main__':
+    run('1,2,3')
