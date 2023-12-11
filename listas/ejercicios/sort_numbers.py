584e2d6e6179708c737bbc0b7f5c6c8cdc730604
Jose Domingo Rodriguez <josedomingo.rguez.rguez@gmail.com>
2023-12-11 14:17:18 +0000

    se termien sort_number

--- listas/ejercicios/sort_numbers.py
+++ listas/ejercicios/sort_numbers.py
@@ -4,8 +4,16 @@
 
 
 def run(numbers: list) -> list:
-    # TU CÓDIGO AQUÍ
-    sorted_numbers = 'output'
+    length_numbers = len(numbers)
+    
+    for element in range(length_numbers):
+        for num_pos in range (0, length_numbers - element - 1):
+            if numbers[num_pos] > numbers[num_pos + 1]:
+                numbers[num_pos] = numbers[num_pos + 1]
+                numbers[num_pos + 1] = numbers[num_pos]
+    
+    sorted_numbers = numbers
+
 
     return sorted_numbers
 
