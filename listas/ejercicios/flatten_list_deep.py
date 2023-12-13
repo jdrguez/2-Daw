adec6c20b3c0cd599ecc1ec4dd249061ce739001
jdrguez <josedomingo.rguez.rguez@gmail.com>
2023-12-13 14:43:33 +0000

    Se modifican los ejercicios

--- listas/ejercicios/flatten_list_deep.py
+++ listas/ejercicios/flatten_list_deep.py
@@ -6,17 +6,27 @@
 def run(elements: list) -> list:
     flatten_elements = []
     for item in elements:
-            if isinstance (item, list):
-                for element in item:
-                    if isinstance (element, list):
-                        flatten_elements.append(element)
-                    else:
-                        flatten_elements.append(element)
-            else:
-                flatten_elements.append(item)
+        if isinstance(item, list):
+            for element in item:
+                if isinstance(element, list):
+                    flatten_elements.append(element)
+                else:
+                    flatten_elements.append(element)
+        else:
+            flatten_elements.append(item)
 
     return flatten_elements
 
 
 if __name__ == '__main__':
-    run([0, 10, [20, [-10, [5, 5, 5], -20], 30], 40, 50, [60, 70, [-1, -2, -3], 80], [90, 100, 110, 120]])
+    run(
+        [
+            0,
+            10,
+            [20, [-10, [5, 5, 5], -20], 30],
+            40,
+            50,
+            [60, 70, [-1, -2, -3], 80],
+            [90, 100, 110, 120],
+        ]
+    )
--- listas/ejercicios/sort_numbers.py
+++ listas/ejercicios/sort_numbers.py
@@ -5,15 +5,14 @@
 
 def run(numbers: list) -> list:
     length_numbers = len(numbers)
-    
+
     for element in range(length_numbers):
-        for num_pos in range (0, length_numbers - element - 1):
+        for num_pos in range(0, length_numbers - element - 1):
             if numbers[num_pos] > numbers[num_pos + 1]:
                 numbers[num_pos] = numbers[num_pos + 1]
                 numbers[num_pos + 1] = numbers[num_pos]
-    
-    sorted_numbers = numbers
 
+    sorted_numbers = numbers
 
     return sorted_numbers
 
