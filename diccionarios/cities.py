2c83c728032f044438931c1cf8113889849b5e72
Jose Domingo Rodriguez <josedomingo.rguez.rguez@gmail.com>
2024-01-10 12:12:02 +0000

    Se termina cities

--- diccionarios/cities.py
+++ diccionarios/cities.py
@@ -4,11 +4,15 @@
 
 
 def run(cinfo: str) -> dict:
-    # TU CÓDIGO AQUÍ
-    cities = 'output'
-
+    cleaned_cinfo = cinfo.split(';')
+    cities = {}
+    for pairs in cleaned_cinfo:
+        city, population = pairs.split(':')
+        cities[city] = int(population)
     return cities
 
 
 if __name__ == '__main__':
     run('Tokyo:38_140_000;Delhi:26_454_000;Shanghai:24_484_000;Mumbai:21_357_000')
+    
+    
