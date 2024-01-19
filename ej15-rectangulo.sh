c0870ba459a2281624a2caa84c3d5ece98437523
jdrguez <145053914+jdrguez@users.noreply.github.com>
2024-01-19 12:35:43 +0000

    Update ej15-rectangulo.sh

--- ej15-rectangulo.sh
+++ ej15-rectangulo.sh
@@ -5,10 +5,14 @@ altura=$2
 
 if [ $# -eq 0 ]
 then
+    echo "ADVERTENCIA: No has puesto argumentos"
+    echo "Se utilizaran por defecto base 7 y altura 4"
     base=7
     altura=4
 elif [ $# -eq 1 ]
 then
+    echo "ADVERTENCIA: Solo se ha indicado la base $base"
+    echo "Se utilizará el valor de altura 4 por defecto"
     base=$1
     altura=4
 fi
@@ -17,8 +21,8 @@ area=$((base * altura))
 
 echo "Se va a pintar un rectángulo de base : $base , altura: $altura y área: $area"
 
-for ((y = 0; y < altura; y++)); do
-    for ((x = 0; x < base; x++)); do
+for y in $(seq 1 $altura); do
+    for x in $(seq 1 $base); do
         echo -n " #"
     done
     echo
