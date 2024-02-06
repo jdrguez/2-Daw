01b5a3563fe24d521e98eca0f34c5c90391828f8
jrguez <josedomingo.rguez.rguez@gmail.com>
2024-02-06 00:15:00 +0000

    Se termina la tarea8

--- Unidad-5/Tarea-8/consultas-bbdd(1).sql
+++ Unidad-5/Tarea-8/consultas-bbdd(1).sql
@@ -90,35 +90,118 @@ nta;
 └────────────┴─────────┴─────────────────┴─────────────┘
 **/
 
--- Encontrar el modelo de coche más caro que ha sido reparado al menos una vez.
+-- Encontrar el modelo de coche más caro.
   -- Cosas que debo de tener en cuenta:
     -- ¿Qué me están pidiendo?. MAX
   
+SELECT modelo, max(precio) as coche_mas_caro FROM coches;
+/**
+┌────────────────┬────────────────┐
+│     modelo     │ coche_mas_caro │
+├────────────────┼────────────────┤
+│ Eléctrico 2021 │ 40000.0        │
+└────────────────┴────────────────┘
+**/
+
 
 -- Mostrar los clientes que han comprado al menos un coche (un coche o más) y la cantidad de coches comprados.
   -- Cosas que debo de tener en cuenta:
     -- ¿Qué me están pidiendo?. COUNT
 
+SELECT  c.nombre,COUNT(v.id_coche) as cantidad_comprada from clientes as c, ventas as v WHERE c.id_cliente=v.id_cliente GROUP BY c.id_cliente, c.nombre HAVING COUNT(v.id_coche) >=1;
+/**
+┌─────────────────┬───────────────────┐
+│     nombre      │ cantidad_comprada │
+├─────────────────┼───────────────────┤
+│ Juan Pérez      │ 1                 │
+│ María Gómez     │ 1                 │
+│ Carlos López    │ 1                 │
+│ Ana Martínez    │ 1                 │
+│ Pedro Rodríguez │ 1                 │
+│ Laura Sánchez   │ 1                 │
+│ Miguel González │ 1                 │
+│ Isabel Díaz     │ 1                 │
+│ Elena Torres    │ 1                 │
+└─────────────────┴───────────────────┘
+**/
+
 -- Encontrar los clientes que han comprado coches de la marca 'Toyota':
   -- Cosas que debo de tener en cuenta:
     -- ¿Qué me están pidiendo?. Like | regexp | =. Tabla normalizada ?.
+SELECT cli.nombre, c.modelo FROM clientes as cli, coches as c, ventas as v WHERE v.id_coche=c.id_coche AND v.id_cliente=cli.id_cliente AND c.modelo REGEXP 'Toyota';
+-- No se obtuvo resultado
 
 -- Calcular el promedio de edad de los clientes que han comprado coches de más de 25,000.
   -- Cosas que debo de tener en cuenta:
     -- ¿Qué me están pidiendo?. 
 
+
+SELECT AVG(cli.edad) as promedio_edad FROM clientes as cli, coches as c, ventas as v WHERE v.id_coche=c.id_coche AND v.id_cliente=cli.id_cliente AND c.precio > (SELECT precio from coches where precio >= 25000);
+/**
+┌──────────────────┐
+│  promedio_edad   │
+├──────────────────┤
+│ 32.8333333333333 │
+└──────────────────┘
+**/
+
 -- Mostrar los modelos de coches y sus precios que fueron comprados por clientes mayores de 30 años.
   -- Cosas que debo de tener en cuenta:
     -- ¿Qué me están pidiendo?.
+
+select c.modelo, c.precio FROM coches as c, clientes as cli, ventas as v WHERE v.id_cliente=cli.id_cliente AND v.id_coche=c.id_coche AND cli.edad >30;
+/**
+┌────────────────┬─────────┐
+│     modelo     │ precio  │
+├────────────────┼─────────┤
+│ SUV 2023       │ 30000.0 │
+│ Camioneta 2023 │ 32000.0 │
+│ Compacto 2021  │ 20000.0 │
+│ Deportivo 2023 │ 35000.0 │
+└────────────────┴─────────┘
+**/
 
 -- Encontrar los coches vendidos en el año 2022 junto con la cantidad total de ventas en ese año.
   -- Cosas que debo de tener en cuenta:
     -- ¿Qué me están pidiendo?.
 
+select c.modelo, c.precio, COUNT(v.id_coche) as cantidad_ventas FROM coches as c, ventas as v WHERE v.id_coche=c.id_coche AND v.fecha_venta regexp '2022' GROUP BY c.id_coche ;
+
+--No se obtuvo resultado
+
 -- Listar los coches cuyos precios son mayores que el precio promedio de coches vendidos a clientes menores de 30 años.
   -- Cosas que debo de tener en cuenta:
     -- ¿Qué me están pidiendo?. AVG
 
+select c.modelo FROM coches as c, clientes as cli, ventas as v WHERE v.id_coche=c.id_coche AND v.id_cliente=cli.id_cliente AND c.precio > (SELECT AVG(precio) from coches as c, clientes as cli, ventas as v WHERE v.id_coche=c.id_coche AND v.id_cliente=cli.id_cliente AND cli.edad <=30);
+
+/**
+┌────────────────┐
+│     modelo     │
+├────────────────┤
+│ SUV 2023       │
+│ Camioneta 2023 │
+│ Deportivo 2023 │
+│ Eléctrico 2021 │
+└────────────────┘
+**/
+
 -- Calcular el total de ventas por marca de coche, ordenado de forma descendente por el total de ventas:
   -- Cosas que debo de tener en cuenta:
     -- ¿Qué me están pidiendo?. COUNT| DESC|ASC precio
+SELECT c.modelo, COUNT(c.id_coche) as total_ventas FROM coches as c, ventas as v WHERE v.id_coche=c.id_coche GROUP BY c.modelo ORDER BY total_ventas DESC;
+/**
+┌────────────────┬──────────────┐
+│     modelo     │ total_ventas │
+├────────────────┼──────────────┤
+│ Sedán 2022     │ 1            │
+│ SUV 2023       │ 1            │
+│ Híbrido 2022   │ 1            │
+│ Hatchback 2021 │ 1            │
+│ Eléctrico 2021 │ 1            │
+│ Deportivo 2023 │ 1            │
+│ Coupé 2022     │ 1            │
+│ Compacto 2021  │ 1            │
+│ Camioneta 2023 │ 1            │
+└────────────────┴──────────────┘
+**/
