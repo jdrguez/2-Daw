42dad5f9e68c3c0fddedc60b1fdbd0c0ac888e29
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-02-08 16:16:56 +0000

    a

--- ficheros/avg_temps.py
+++ ficheros/avg_temps.py
@@ -8,21 +8,20 @@ from pathlib import Path
 def run(input_path: Path) -> bool:
     output_path = 'data/avg_temps/avg_temps.dat'
     f_ipath = open(input_path)
-    f_opath = open(output_path, 'w')
     total_tempertatures = 0
-    
-    with open(input_path) as f_ipath:
+
+    with open(output_path, "w") as f_opath:
         for month in f_ipath:
             temperatures = month.strip().split(',')
             total_tempertatures = 0
             for day in temperatures:
                 total_tempertatures += int(day)
-            
+
             media = total_tempertatures / len(temperatures)
             f_opath.write(f'{media:.2f}\n')
 
-    return filecmp.cmp(output_path, 'data/avg_temps/.expected', shallow=False)
+    return filecmp.cmp(output_path, './data/avg_temps/.expected', shallow=False)
 
 
 if __name__ == '__main__':
-    run('data/avg_temps/temperatures.dat')
+    run(Path('data/avg_temps/temperatures.dat'))
