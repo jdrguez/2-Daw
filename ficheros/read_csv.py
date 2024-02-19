8b6eaa8e4017f5ed703212179520a1c7f8a39abf
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-02-19 14:33:03 +0000

    !

--- ficheros/read_csv.py
+++ ficheros/read_csv.py
@@ -13,11 +13,6 @@ def run(datafile: Path) -> list:
             cdata[k] = 0
     f.close()
 
-    with open(datafile) as f1:
-        for line in f1:
-            cleaned_line = line.strip().split(',')
-            for word in cleaned_line:
-                cdata[k] = cdata.get(word)
     data = [cdata]
     return data
 
