32b0c6b42dcaa4083936d2ed126f1ef68cfce4b1
Jose Domingo Rodriguez <josedomingo.rguez.rguez@gmail.com>
2024-02-24 16:33:13 +0000

    Se agrega scrip de procesos

---
+++ linux/scrip_Procesos/Script_Gestion_Procesos-1DAW-K.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+######################################################################
+#   Nombre: Gestión de procesos                                      #
+#   Autores: José Domingo Rguez y Lili                               #
+#                                                                    #
+#   Objetivo: Crear un menú para analizar los procesos que consuman  #
+#   los recursos indicados.                                          #
+#                                                                    #
+#   Entrada: Parametros de MaxCpu y MaxMem                           #
+#   Salida: Monstrar los procesos y con su correspondiente menú      #
+#                                                                    #
+#   Fecha: 22/04/2024                                                #
+#                                                                    #
+#    Versión: 1.0                                                    #
+######################################################################
+
+
+maxCPU=$1
+maxMem=$2
+
+
+trap "echo Se procede a interrumpir la ejecución..; exit 0"
+INT
+trap "echo Finalizando la ejecución del script...; exit 0" TERM
+trap "echo Carga del sistema; uptime" USR1
+trap "echo Límite del proceso; unlimit -u" USR2
+
+
+
+if [ "$#" -ne 2 ]; 
+then
+    echo "ERROR 100: Debes introducir dos argumentos para que el programa pueda funcionar "
+    exit 100
+fi
+
+if [ "$1" -lt 0 ] || [ "$2" -lt 0 ];
+then 
+    echo "ERROR 200: Los valores introducidos no son correctos, estos son inferiores a 0"
+    exit 200
+fi
+
+if [ "$2" -gt 100 ];
+then 
+    echo "ERROR 150: $maxMen es mayor al 100% del cpu. Introduzca valores correctos"
+    exit 150
+fi
+
+while true
+do
+
+    echo "Estamos comprobando tanto la CPU como la memoria del sistema..."
+    echo "Espere, por favor ... "
+
+    sleep 3
+
+    procesosCPU=$(ps axo pcpu= --sort -pcpu | head -n 1)
+    piofidCPUprocesos=$(ps axo pid= --sort -pcpu | head -n 1)
+
+    procesosMEMORIA=$(ps axo pmem= --sort -pnem | head -n 1)
+    pidMEMORIAprocesos=$(ps axo pid= --sort -pmem | head -n 1)
+
+    if echo "$procesosCPU >= $maxCPU" | bc -l | grep -q 1 && echo "$procesosMEMORIA > $maxMem" | bc -l | grep -q 1;
+    then
+        echo "Los procesos obtenidos superen los limites establecidos en un inicio, porfavor determina que proceso debemos tratar: "
+        read -p "Seleccione entre los siguientes = CPU o Memoria" opcion
+        if [[ "$opcion" == "CPU" ]]
+        then
+            echo "Has seleccido  la opción de verificar la carga en CPU."
+            ps p $piofidCPUprocesos
+            break
+        fi
+        
+        if [[ "$opcion" == "Memoria" ]]
+        then
+            echo "Has seleccionado la opción de verificar la carga en Memoria."
+            ps p  $pidMEMORIAprocesos
+            break
+        fi 
+
+        if [[ -z "$opcion" ]]
+        then
+            echo "Introduce un valor correcto"
+        fi
+done
+
+echo ""
+echo "*************************"
+echo "**** O P C I O N E S ****"
+echo "*************************"
+echo ""
+echo " a) Ignorar el aviso y seguir comprobando"
+echo " b) Disminuir en 3 puntos la prioridad del proceso"
+echo " c) Interrumpir el proceso"
+echo " d) Terminar el proceso"
+echo " e) Finalizar inmediatamente el proceso"
+echo " f) Detener el proceso (congelarlo de forma evitable)"
+echo " g) Detener el proceso (congelarlo de forma inevitable)"
+
+read -p "Introduzca la opción que quiere realizar con su proceso: " choice
+
+case "$choice" in
+    a|A|1) echo "Ignoramos el aviso y seguimos comprobando..."
+    ;;
+    b|B|2) echo "Disminuir en 3 puntos la prioridad del proceso"
+    aux=$(( aux + 3 ))
+    renice $aux $1
+    ;;
+    c|C|3) echo "Interrumpir el proceso"
+    kill -INT $1
+    ;;
+    d|D|4) echo "Terminando el proceso"
+    kill -TERM $1
+    ;;
+    e|E|5) echo "Finalizando inmediatamente el proceso"
+    kill -KILL $1
+    ;;
+    f|F|6) echo "Deteniendo el proceso, pero no es reversible."
+    kill -TSTP $1
+    ;;
+    g|G|7) echo "Deteniendo el proceso definitivamente. No se puede revertir esta acción."
+    kill -STOP $1
+    ;;
+    *) echo "Opción incorrecta. $choice no es una opción correcta."
+    ;;
+esac
+
+
+
