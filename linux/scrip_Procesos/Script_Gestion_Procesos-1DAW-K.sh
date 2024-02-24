75841e72521d6d01940c20b4cc4c15bee557093d
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-02-24 17:34:53 +0000

    Se termina el scrip

--- linux/scrip_Procesos/Script_Gestion_Procesos-1DAW-K.sh
+++ linux/scrip_Procesos/Script_Gestion_Procesos-1DAW-K.sh
@@ -20,11 +20,120 @@ maxCPU=$1
 maxMem=$2
 
 
-trap "echo Se procede a interrumpir la ejecución..; exit 0"
-INT
+trap "echo Se procede a interrumpir la ejecución..; exit 0" INT
 trap "echo Finalizando la ejecución del script...; exit 0" TERM
 trap "echo Carga del sistema; uptime" USR1
 trap "echo Límite del proceso; unlimit -u" USR2
+comprobacion_sistema(){
+    while true
+    do
+
+        echo "Estamos comprobando tanto la CPU como la memoria del sistema..."
+        echo "Espere, por favor ... "
+
+        sleep 2
+        clear
+
+        procesosCPU=$(ps axo pcpu= --sort -pcpu | head -n 1)
+        pidCPUprocesos=$(ps axo pid= --sort -pcpu | head -n 1)
+
+        procesosMEMORIA=$(ps axo pmem= --sort -pmem | head -n 1)
+        pidMEMORIAprocesos=$(ps axo pid= --sort -pmem | head -n 1)
+
+        if echo "$procesosCPU >= $maxCPU" | bc -l | grep -q 1 && echo "$procesosMEMORIA > $maxMem" | bc -l | grep -q 1;
+        then
+            echo "Los procesos $pidCPUprocesos y $pidMEMORIAprocesos  se han sobrepasado los limites"
+            read -p "Seleccione entre los siguientes CPU(1,a) o Memoria(2,b): " opcion
+            
+            case "$opcion" in
+                CPU|cpu|1|a) echo "Has elegido coger el proceso en CPU"
+                ps p $pidCPUprocesos
+                menu
+                break
+                ;;
+            
+                Memoria|memoria|mem|b|2) echo "Has elegido coger el proceso en memoria"
+                ps p  $pidMEMORIAprocesos
+                menu
+                break
+                ;;
+
+                *) echo "$opcion no es una opción válida"
+                break
+                ;;
+            esac
+        elif echo "$procesosCPU >= $maxCPU" | bc -l | grep -q 1
+        then 
+            ps p $pidCPUprocesos
+            menu $pidCPUprocesos
+            break
+        elif echo "$procesosMEMORIA >= $maxMem" | bc -l | grep -q 1
+        then
+            ps p $pidMEMORIAprocesos
+            menu $pidMEMORIAprocesos
+            break
+        fi
+
+        sleep 6
+
+    done
+}
+
+
+
+
+
+
+menu(){
+    echo ""
+    echo "*************************"
+    echo "**** O P C I O N E S ****"
+    echo "*************************"
+    echo ""
+    echo " a) Ignorar el aviso y seguir comprobando"
+    echo " b) Disminuir en 3 puntos la prioridad del proceso"
+    echo " c) Interrumpir el proceso"
+    echo " d) Terminar el proceso"
+    echo " e) Finalizar inmediatamente el proceso"
+    echo " f) Detener el proceso (congelarlo de forma evitable)"
+    echo " g) Detener el proceso (congelarlo de forma inevitable)"
+
+
+    while true
+    do
+        read -p "Introduzca la opción que quiere realizar con su proceso: " choice
+        case "$choice" in
+            a|A|1) echo "Ignoramos el aviso y seguimos comprobando..."
+            comprobacion_sistema
+            ;;
+            b|B|2) echo "Disminuir puntos la prioridad del proceso"
+            read -p "Cuantos puntos quieres disminuir el proceso" puntos
+            renice $puntos $pidCPUprocesos
+            break
+            ;;
+            c|C|3) echo "Interrumpir el proceso"
+            kill -INT $pidCPUprocesos
+            break
+            ;;
+            d|D|4) echo "Terminando el proceso"
+            kill -TERM $pidCPUprocesos
+            break
+            ;;
+            e|E|5) echo "Finalizando inmediatamente el proceso"
+            kill -KILL $pidCPUprocesos
+            break
+            ;;
+            f|F|6) echo "Deteniendo el proceso, pero no es reversible."
+            kill -TSTP $pidCPUprocesos
+            ;;
+            g|G|7) echo "Deteniendo el proceso definitivamente. No se puede revertir esta acción."
+            kill -STOP $pidCPUprocesos
+            ;;
+            *) echo "Opción incorrecta. $choice no es una opción correcta."
+            ;;
+        esac
+    done
+}
 
 
 
@@ -46,84 +155,6 @@ then
     exit 150
 fi
 
-while true
-do
-
-    echo "Estamos comprobando tanto la CPU como la memoria del sistema..."
-    echo "Espere, por favor ... "
-
-    sleep 3
-
-    procesosCPU=$(ps axo pcpu= --sort -pcpu | head -n 1)
-    piofidCPUprocesos=$(ps axo pid= --sort -pcpu | head -n 1)
-
-    procesosMEMORIA=$(ps axo pmem= --sort -pnem | head -n 1)
-    pidMEMORIAprocesos=$(ps axo pid= --sort -pmem | head -n 1)
-
-    if echo "$procesosCPU >= $maxCPU" | bc -l | grep -q 1 && echo "$procesosMEMORIA > $maxMem" | bc -l | grep -q 1;
-    then
-        echo "Los procesos obtenidos superen los limites establecidos en un inicio, porfavor determina que proceso debemos tratar: "
-        read -p "Seleccione entre los siguientes = CPU o Memoria" opcion
-        if [[ "$opcion" == "CPU" ]]
-        then
-            echo "Has seleccido  la opción de verificar la carga en CPU."
-            ps p $piofidCPUprocesos
-            break
-        fi
-        
-        if [[ "$opcion" == "Memoria" ]]
-        then
-            echo "Has seleccionado la opción de verificar la carga en Memoria."
-            ps p  $pidMEMORIAprocesos
-            break
-        fi 
-
-        if [[ -z "$opcion" ]]
-        then
-            echo "Introduce un valor correcto"
-        fi
-done
-
-echo ""
-echo "*************************"
-echo "**** O P C I O N E S ****"
-echo "*************************"
-echo ""
-echo " a) Ignorar el aviso y seguir comprobando"
-echo " b) Disminuir en 3 puntos la prioridad del proceso"
-echo " c) Interrumpir el proceso"
-echo " d) Terminar el proceso"
-echo " e) Finalizar inmediatamente el proceso"
-echo " f) Detener el proceso (congelarlo de forma evitable)"
-echo " g) Detener el proceso (congelarlo de forma inevitable)"
-
-read -p "Introduzca la opción que quiere realizar con su proceso: " choice
-
-case "$choice" in
-    a|A|1) echo "Ignoramos el aviso y seguimos comprobando..."
-    ;;
-    b|B|2) echo "Disminuir en 3 puntos la prioridad del proceso"
-    aux=$(( aux + 3 ))
-    renice $aux $1
-    ;;
-    c|C|3) echo "Interrumpir el proceso"
-    kill -INT $1
-    ;;
-    d|D|4) echo "Terminando el proceso"
-    kill -TERM $1
-    ;;
-    e|E|5) echo "Finalizando inmediatamente el proceso"
-    kill -KILL $1
-    ;;
-    f|F|6) echo "Deteniendo el proceso, pero no es reversible."
-    kill -TSTP $1
-    ;;
-    g|G|7) echo "Deteniendo el proceso definitivamente. No se puede revertir esta acción."
-    kill -STOP $1
-    ;;
-    *) echo "Opción incorrecta. $choice no es una opción correcta."
-    ;;
-esac
-
+comprobacion_sistema
 
 
