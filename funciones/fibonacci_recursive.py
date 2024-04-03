13a4667aa2c0129d96ca310da0418b6b957827db
Jose Domingo Rodriguez <josedomingo.rguez.rguez@gmail.com>
2024-04-03 12:02:02 +0100

    Se añade el ultimo

---
+++ funciones/fibonacci_recursive.py
@@ -0,0 +1,12 @@
+# *******************
+# FIBONACCI RECURSIVO
+# *******************
+
+
+def fibonacci(n: int) -> int:
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
