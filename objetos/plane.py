73fa2fe422eecf03e91cfdb4b77802972cbccb3b
Jose Domingo Rodriguez <josedomingo.rguez.rguez@gmail.com>
2024-04-18 14:17:13 +0100

    Se añade plane.py

--- objetos/plane.py
+++ objetos/plane.py
@@ -1,5 +1,6 @@
 class Plane:
     BLACKBOX = True
+    count = 0
     def __init__(self, fuel_capacity: float, max_passengers: int, storage_capacity: float,
                  manufacturer: str, model: str, reactor: bool = True):
         self.fuel_capacity = fuel_capacity
@@ -13,3 +14,20 @@ class Plane:
     def close_doors(self):
         self.doors = {'d1': False, 'd2': False, 'd3': False, 'd4': False, 'd5': False, 'd6': False}
     
+    def remaining_storage_cpty(self, current_load: float) -> float:
+        return self.storage_capacity - current_load
+
+    def requires_refuel(self, distance_to_travel: float, fuel_consumption_per_km: float) -> float:
+        required_fuel = distance_to_travel * fuel_consumption_per_km
+        return required_fuel > self.fuel_capacity
+    
+    def calculate_distance_fuel(distance: float, fuel_consumption_per_km: float) -> float:
+        return distance * fuel_consumption_per_km
+    
+    @staticmethod
+    def get_types_of_plane() -> tuple[str]:
+        return ('Passenger', 'Cargo', 'Military')
+    
+    @classmethod
+    def total_planes(cls) -> None:
+        print(f'{cls.count} total planes built!!!')
