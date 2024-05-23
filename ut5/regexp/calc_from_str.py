9dae5daad430c4b657da5f4a9fd42617f4741b19
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-05-23 15:53:02 +0100

    Se modifica la expresion de calc

--- ut5/regexp/calc_from_str.py
+++ ut5/regexp/calc_from_str.py
@@ -2,9 +2,8 @@ import re
 
 
 def calc(expression: str) -> int | float:
-    valid_expresion = r'(\d+)([+\-*/])(\d+)'
+    valid_expresion = r'(\d+)\s*([+\-*/])\s*(\d+)'
     result = 0
-    expression = expression.replace(' ', '')
     if m := re.match(valid_expresion, expression):
         num1, operator, num2 = m.groups()
         num1, num2 = int(num1), int(num2)
