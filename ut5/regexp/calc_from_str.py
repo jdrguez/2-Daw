0c4d48a818ef32dcd128e26895c2cab65f85ed2f
Jose Domingo Rodriguez <josedomingo.rguez.rguez@gmail.com>
2024-05-23 09:44:48 +0100

    Se termina calc

---
+++ ut5/regexp/calc_from_str.py
@@ -0,0 +1,22 @@
+import re
+
+
+def calc(expression: str) -> int | float:
+    valid_expresion = r'(\d+)([+\-*/])(\d+)'
+    result = 0
+    expression = expression.replace(' ', '')
+    if m:= re.match(valid_expresion, expression):
+        num1, operator, num2 = m.groups()
+        num1, num2 = int(num1), int(num2)
+        match operator:
+            case '+':
+                result =  num1 + num2
+            case '-':
+                result =  num1 - num2
+            case '*':
+                result =  num1 * num2
+            case '/':
+                result =  num1 / num2
+            case _ :
+                result = None
+    return result
