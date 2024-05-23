8d54314bb68556107564ef27805b1c8489662a88
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-05-23 15:26:42 +0100

    S

--- ut5/regexp/valid_float.py
+++ ut5/regexp/valid_float.py
@@ -2,5 +2,5 @@ import re
 
 
 def is_valid_float(number: str) -> bool:
-    valid_float = r'(-*\d)*[.e]+\w*'
+    valid_float = r'(-*\d)*[.e]+[\d|_]*'
     return re.fullmatch(valid_float, number) is not None
--- ut5/regexp/valid_url.py
+++ ut5/regexp/valid_url.py
@@ -2,7 +2,5 @@ import re
 
 
 def is_valid_url(url: str) -> bool:
-    valid_form = r'^https?:\//.+\.+.+\w'
-    if m := re.fullmatch(valid_form, url):
-        return True
-    return False
+    valid_form = r'https?:\//\w+(\.\w+)+([/.]*\w*)*'
+    return re.fullmatch(valid_form, url) is not None
