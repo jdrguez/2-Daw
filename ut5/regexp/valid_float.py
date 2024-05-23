690788151aa82431bfc3820009325918fa08b74a
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-05-23 15:47:17 +0100

    Se corrige

--- ut5/regexp/valid_float.py
+++ ut5/regexp/valid_float.py
@@ -2,5 +2,5 @@ import re
 
 
 def is_valid_float(number: str) -> bool:
-    valid_float = r'(-*\d)*[.e]+[\d|_]*'
+    valid_float = r'(-*\d_?)*[.e]+[\d|_]*'
     return re.fullmatch(valid_float, number) is not None
