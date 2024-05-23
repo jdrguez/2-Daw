75e847bf176bff997b97bde4a6843c70a5e619fd
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-05-23 15:28:23 +0100

    Se terminan los ejercicios

--- ut5/regexp/valid_url.py
+++ ut5/regexp/valid_url.py
@@ -3,6 +3,4 @@ import re
 
 def is_valid_url(url: str) -> bool:
     valid_form = r'^https?:\//.+\.+.+\w'
-    if m := re.fullmatch(valid_form, url):
-        return True
-    return False
+    return re.fullmatch(valid_form, url) is not None
