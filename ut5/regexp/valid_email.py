d1224068369a0c96f5f095cfaf4e4c8b4e75b042
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-05-31 17:14:21 +0100

    Se empieza sqlite

--- ut5/regexp/valid_email.py
+++ ut5/regexp/valid_email.py
@@ -2,7 +2,7 @@ import re
 
 
 def is_valid_email(email: str) -> bool:
-    valid_email = r'(\w|-)+@+\w+([.]\w+)+'
+    valid_email = r'[\w-]+@\w+([.]\w+)+'
     return re.fullmatch(valid_email, email) is not None
 
 
