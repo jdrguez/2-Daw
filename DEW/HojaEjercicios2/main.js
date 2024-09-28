ce733902db710be3c6e507e4b26c3b508ac07578
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-09-28 23:17:24 +0100

    Se termina la hoja n2

--- DEW/HojaEjercicios2/main.js
+++ DEW/HojaEjercicios2/main.js
@@ -16,10 +16,30 @@ function funcion1(){
 
 //Ejercicio2
 
-function funcion2(){
+function funcion2() {
+    const input = document.getElementById("numeros").value;
+    const valores = input.split(',').map(item => item.trim());
+    let suma = 0;
+    let count = 0;
 
+    valores.forEach((valor, index) => {
+        const num = parseFloat(valor);
+        if (!isNaN(num)) {
+            suma += num;
+            count++;
+        } else {
+            console.log(`El argumento número ${index + 1} "${valor}" no es un número, lo ignoramos.`);
+        }
+    });
+
+    if (count > 0) {
+        const media = suma / count;
+        console.log(`La suma de los números es: ${suma}`);
+        console.log(`La media de los números es: ${media}`);
+    } else {
+        console.log("No se ingresaron números válidos.");
+    }
 }
-
 
 //Ejercicio 3
 function funcion3(){
@@ -63,11 +83,53 @@ function funcion5(){
 
 }
 
+// Ejercicio 6
 
-//Ejercicio 6
+function funcion6() {
+    let csvText = document.getElementById("csvText").value;
+    let delimiter = document.getElementById("delimiter").value;
+
+    if (delimiter === "") {
+        delimiter = ",";
+    }
+    let elementos = csvText.split(delimiter);
+    console.log(`Se han detectado ${elementos.length} elementos:`);
+    for (let i = 0; i < elementos.length; i++) {
+        console.log(`Elemento ${i + 1}: ${elementos[i].trim()}`);
+    }
+}
+
 //Ejercicio 7
+
+function funcion7() {
+    let texto = document.getElementById("text7").value;
+    texto = texto.toLowerCase().trim();
+    let puntos = 0;
+    if (texto.startsWith("el ") || texto.startsWith("la ")) {
+        puntos += 1;
+    }
+    if (texto.endsWith("ando") || texto.endsWith("endo")) {
+        puntos += 10;
+    }
+    let regexCon = /\b\w+\scon\s\w+\b/;
+    if (regexCon.test(texto)) {
+        puntos += 100;
+    }
+    console.log(`El total de puntos es: ${puntos}`);
+}
+
 //Ejercicio 8
 
+function funcion8() {
+    let texto = document.getElementById("text8").value;
+    if (texto.includes("arriba")) {
+        let nuevoTexto = texto.replace(/arriba/g, "abajo");
+        console.log("La cadena 'arriba' fue encontrada.");
+        console.log("El nuevo texto es:", nuevoTexto);
+    } else {
+        console.log("La cadena 'arriba' no fue encontrada en el texto.");
+    }
+}
 
 
 // Ejercicio 9
@@ -88,3 +150,13 @@ function funcion9(){
 }
 
 //Ejercicio 10
+function funcion10() {
+    let frase = document.getElementById("text10").value;
+    let fraseLimpia = frase.toLowerCase().replace(/[\W_]/g, '');
+    let fraseInvertida = fraseLimpia.split('').reverse().join('');
+    if (fraseLimpia === fraseInvertida) {
+        console.log("La frase es un palíndromo.");
+    } else {
+        console.log("La frase no es un palíndromo.");
+    }
+}
