5ea57ad6ecbdc033b00432315ea773d583e2fe03
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-10-21 18:06:01 +0100

    Fix edit form

--- DSW/supertodo/tasks/forms.py
+++ DSW/supertodo/tasks/forms.py
@@ -7,3 +7,21 @@ class AddTaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ('title', 'content', 'done', 'complete_before')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+
+class EditPostForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ('title', 'content', 'done', 'complete_before')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
--- DSW/supertodo/tasks/views.py
+++ DSW/supertodo/tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.utils.text import slugify
 
-from .forms import AddTaskForm
+from .forms import AddTaskForm, EditPostForm
 from .models import Task
 
 # Create your views here.
@@ -34,7 +34,7 @@ def add_task(request):
             return redirect('tasks:task-list')
     else:
         form = AddTaskForm()
-    return render(request, 'tasks/add.html', dict(form=form))
+    return render(request, 'tasks/modifiers/add.html', dict(form=form))
 
 
 def delete_task(request):
@@ -42,17 +42,22 @@ def delete_task(request):
 
 
 def edit_task(request, task_slug: str):
-    task = Task.objects.get('task_slug')
+    task = Task.objects.get(slug=task_slug)
+
     if request.method == 'POST':
-        if (form := AddTaskForm(request.POST)).is_valid():
+        if (form := EditPostForm(request.POST, instance=task)).is_valid():
             task = form.save(commit=False)
+
             task.slug = slugify(task.title)
-            print('funciona')
+
             task.save()
+
             return redirect('tasks:task-list')
+
     else:
-        form = AddTaskForm(initial=task)
-    return render(request, 'tasks/add.html')
+        form = EditPostForm(instance=task)
+
+    return render(request, 'tasks/modifiers/edit.html', dict(task=task, form=form))
 
 
 def task_detail(request, task_slug: str):
