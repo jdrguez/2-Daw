a046fa7b73b29ba72565b81db6d99427f6bd9ce9
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-10-30 20:11:57 +0000

    fix css

--- DSW/tribu/echos/forms.py
+++ DSW/tribu/echos/forms.py
@@ -7,6 +7,9 @@ class AddEchoForm(forms.ModelForm):
     class Meta:
         model = Echo
         fields = ['content', 'user']
+        widgets = {
+            'content': forms.TextInput(attrs={'id': 'post-title'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
