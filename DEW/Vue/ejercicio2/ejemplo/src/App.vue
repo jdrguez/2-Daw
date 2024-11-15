288f9d8b6a653bd5d7a3cd9aac333ae971606c85
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-11-15 20:29:14 +0000

    vue

---
+++ DEW/Vue/ejercicio2/ejemplo/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import About from './components/About.vue';
+
+
+
+
+</script>
+
+<template>
+  <Navbar></Navbar>
+
+  <main>
+    <router-view/>
+  </main>
+
+</template>
+
+
+<style scoped>
+</style>
